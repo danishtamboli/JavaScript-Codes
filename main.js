a46f50c20075ef9bbc2548a89ced24e10f75ec52
Danish Tamboli <70634416+danishtamboli@users.noreply.github.com>
2026-03-31 09:50:30 +0530

    Implement throttle(fn, delay).
    Implement throttle(fn, delay).

--- main.js
+++ main.js
@@ -1,39 +1,17 @@
-class LRUCache {
-  constructor(capacity) {
-    this.capacity = capacity;
-    this.cache = new Map();
-  }
+function throttle(fn, delay) {
+  let lastCall = 0;
 
-  get(key) {
-    if (!this.cache.has(key)) return -1;
+  return function (...args) {
+    const now = Date.now();
 
-    const value = this.cache.get(key);
-    this.cache.delete(key);        // remove
-    this.cache.set(key, value);    // re-insert (most recently used)
-
-    return value;
-  }
-
-  put(key, value) {
-    if (this.cache.has(key)) {
-      this.cache.delete(key);
+    if (now - lastCall >= delay) {
+      lastCall = now;
+      fn.apply(this, args);
     }
-
-    this.cache.set(key, value);
-
-    if (this.cache.size > this.capacity) {
-      const firstKey = this.cache.keys().next().value; // least recently used
-      this.cache.delete(firstKey);
-    }
-  }
+  };
 }
+const log = () => console.log("Called");
 
-const lru = new LRUCache(2);
+const throttledLog = throttle(log, 2000);
 
-lru.put(1, 10);
-lru.put(2, 20);
-lru.get(1);       // 10 (now 1 is most recently used)
-lru.put(3, 30);   // removes key 2
-
-console.log(lru.get(2)); // -1
-console.log(lru.get(3)); // 30
+window.addEventListener("scroll", throttledLog);
