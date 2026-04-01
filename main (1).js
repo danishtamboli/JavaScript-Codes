510ca198ebb3e2cf585788174cf35bbf615ec271
Danish Tamboli <70634416+danishtamboli@users.noreply.github.com>
2026-04-01 10:16:21 +0530

    Implement debounce(fn, delay).
    Implement debounce(fn, delay).

--- main (1).js
+++ main (1).js
@@ -1,9 +1,22 @@
-function capitalizeWords(str) {
-  return str
-    .split(" ")
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-    .join(" ");
-}
-const input = "hello world from javascript";
+function debounce(fn, delay) {
+  let timer;
 
-console.log(capitalizeWords(input));
+  return function (...args) {
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
+}
+function search(query) {
+  console.log("Learning:", query);
+}
+
+const debouncedSearch = debounce(search, 500);
+
+// Simulate typing
+debouncedSearch("r");
+debouncedSearch("re");
+debouncedSearch("rea");
+debouncedSearch("react");
